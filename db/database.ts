9ba382cd70295d24680cc595cb35c90e85b0b241
aliamerj <aliamer19ali@gmail.com>
2024-06-03 16:45:30 +0300

    fix Can't find meta/_journal.json file

--- db/database.ts
+++ db/database.ts
@@ -3,6 +3,7 @@ import postgres from "postgres";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import * as users from "./schemas/users";
 import * as blogs from "./schemas/blogs";
+import path from "path";
 const schema = {
   ...users,
   ...blogs,
@@ -12,5 +13,5 @@ const connection = process.env.DRIZZLE_DATABASE_URL as string;
 const queryClient = postgres(connection, { max: 1 });
 export const databaseDrizzle = drizzle(queryClient, { schema });
 export const migrationClient = migrate(databaseDrizzle, {
-  migrationsFolder: "drizzle",
+  migrationsFolder: path.join("drizzle"),
 });
